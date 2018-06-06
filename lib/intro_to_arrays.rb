a99492b31d2bbdd0def7fc474ecd6c6edbfe062c
Rebecca Govert <becci.govert@gmail.com>
2018-06-06 03:56:52 +0000

    automatically backed up by learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,12 @@
+def instantiate_new_array
+  my_new_array =  []
+end
+
+def array_with_two_elements
+ my_two_array = ["object 1", "object 2"]
+end
+  
+def first_element(array, i)
+  my_first_element = first_element(array)
+  return first_element[0]
+end
